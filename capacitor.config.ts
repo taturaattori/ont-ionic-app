fc931870af31640046a3970b34369e0842c2e71b
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2024-04-22 14:38:35 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'task-manager-app',
+  webDir: 'dist'
+};
+
+export default config;
